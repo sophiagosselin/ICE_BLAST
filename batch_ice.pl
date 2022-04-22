c465ebe95d25402781a8f64522f5bdec3ffebd3b
sophia.gosselin <sophia.gosselin@uconn.edu>
2022-04-22 16:28:47 -0400

    Better batch code for easy parameter testing!

--- batch_ice.pl
+++ batch_ice.pl
@@ -11,7 +11,33 @@ foreach my $evalue (@evals){
     my $dir = "$evalue\_$id";
     mkdir($dir);
     copy("intein.fasta","$dir/intein.fasta");
-    copy("ice.sh","$dir/ice.sh");
     copy("iceblast_v1.1.pl","$dir/iceblast_v1.1.pl");
+    open(SH, "+> $dir/ice.sh");
+    print SH "#!/usr/bin/env bash
+#SBATCH --job-name=ice_intein
+#SBATCH --nodes=1
+#SBATCH --partition=xeon
+#SBATCH --qos=general
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=16
+#SBATCH --mem=128gb
+#SBATCH -t 100:00:00
+#SBATCH --chdir=$dir
+#SBATCH --mail-type=END
+#SBATCH --mail-user=sophia.gosselin\@uconn.edu
+#SBATCH -o ice_ds_\%j.out
+#SBATCH -e ice_ds_\%j.err
+
+module load blast/2.11.0
+module load perl/5.30.1
+module load uclust
+#put paths for the databases you intend to use here.
+export BLASTDB=\$BLASTDB:/labs/Gogarten/ActinophagesSummer21
+export BLASTDB=\$BLASTDB:/home/FCAM/sgosselin/uniref50
+
+#refer to the perl script or -H for information on options invoked
+perl iceblast_v1.1.pl -in intein.fasta -psidb uniref50.fasta -outdb allPhams.faa -t 8 -id $id -ds 1 -e $evalue";
+    close SH;
+    system("sbatch $dir/ice.sh");
   }
 }
