900c5ad95e0efc33e5a84aeaf4baacc79ca0e974
sophia.gosselin <sophia.gosselin@uconn.edu>
2023-04-17 15:11:58 -0400

    Revised batch script for slurm to work with new systems.

--- ice_blast.sh
+++ ice_blast.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
+
+#This code will run ICE BLAST on as a job on a slurm interface
+
+#This is the SBATCH block.
 #SBATCH --job-name=ice_GPD
 #SBATCH --nodes=1
 #SBATCH --qos=general
@@ -7,7 +11,7 @@
 #SBATCH --mem=64gb
 #SBATCH -t 100:00:00
 #SBATCH --mail-type=END
-#SBATCH --mail-user=sophia.gosselin@uconn.edu
+#SBATCH --mail-user=YOUR_EMAIL_ADDRESS@WHATEVER.COM
 #SBATCH -o ice_blast_%j.out
 #SBATCH -e ice_blast_%j.err
 
@@ -19,7 +23,11 @@ module load uclust
 #put paths for the databases you intend to use here.
 export BLASTDB=$BLASTDB:/home/FCAM/sgosselin/22_08_01_GPD_ice/database
 export BLASTDB=$BLASTDB:/home/FCAM/sgosselin/uniref50
+#you most likely don't need to add these exports, but if ICEBLAST can't find your DB
+#and you've already given it the full path to the DB try adding that path here!
+export PATH=~/home/FCAM/sgosselin/22_08_01_GPD_ice/database:$PATH
+export PATH=~/home/FCAM/sgosselin/uniref50:$PATH
 
 #refer to the perl script or -H for information on options invoked
-perl iceblast.pl -in intein_centroids.fasta -psidb uniref50.fasta -outdb GPD_proteome.faa -t 16 -id 0.60 -e 1e-20 -ds
-
+#make sure to include full path to databases, since ICE BLAST checks them for R/W privs
+perl iceblast.pl -in intein_centroids.fasta -psidb /home/FCAM/sgosselin/uniref50/uniref50.fasta -outdb /home/FCAM/sgosselin/22_08_01_GPD_ice/database/GPD_proteome.faa -t 16 -id 0.60 -e 1e-20 -ds
